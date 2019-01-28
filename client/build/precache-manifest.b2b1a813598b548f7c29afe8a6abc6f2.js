dfb1dadac83ca63b9ff56dffb4185d8df3e46974
daniel <you@example.com>
2019-01-27 20:51:16 -0800

    Fixed file issue

---
+++ client/build/precache-manifest.b2b1a813598b548f7c29afe8a6abc6f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0146b8ed04ac4f322b90",
+    "url": "/static/css/main.ccd5181c.chunk.css"
+  },
+  {
+    "revision": "0146b8ed04ac4f322b90",
+    "url": "/static/js/main.0146b8ed.chunk.js"
+  },
+  {
+    "revision": "bc31853c80bdd68c51b6",
+    "url": "/static/js/1.bc31853c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86c5b9d06d1d6b14932321caaea9873b",
+    "url": "/index.html"
+  }
+];
